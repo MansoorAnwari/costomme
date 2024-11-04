a1468873df734011b20a486fd3804a213c83684c
Mansoor Anwari <mansooranwary50@gmail.com>
2024-11-04 23:10:46 +0330

    feat(component): setup login form with validation
    - Installed formik and yup for form validation
    - Created  folder and added  file to handle input errors using regex
    - Added  folder with , , , and  files
    - Completed login page with validated form

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,15 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "./pages/Root";
 import Index from "./pages/Home";
-import NotFound from "./pages/NotFound.jsx";
+import NotFound from "./pages/NotFound";
+import LoginPage from "./pages/Login";
 
 const router = createBrowserRouter([
     {
         path: "/",
         element: <Root />,
         children: [
-            { path: "", element: <Index /> }
+            { path: "", element: <Index /> },
+            {path: "login", element: <LoginPage /> },
         ],
+
         errorElement: <NotFound />
     }
 ]);
